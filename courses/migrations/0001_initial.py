ecd1dc89b5548011450dab4b6402db2691ba1738
Anabell <gonzalez.anabell@gmail.com>
2016-01-20 19:09:20 -0500

    added models and made migration

---
+++ courses/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Courses',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('description', models.TextField(max_length=200)),
+                ('price', models.IntegerField(default=0)),
+                ('location', models.TextField(max_length=500)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Order',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('order_status', models.IntegerField(default=0)),
+                ('courses', models.ManyToManyField(to='courses.Courses')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,14 @@
 from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import User
+
+class Courses(models.Model):
+	name = models.CharField(max_length=200)
+	description = models.TextField(max_length=200)
+	price = models.IntegerField(default = 0)
+	location = models.TextField(max_length=500)
+
+class Order(models.Model):
+	order_status = models.IntegerField(default=0)
+	courses = models.ManyToManyField(Courses)
+	user = models.ForeignKey(User)
+
